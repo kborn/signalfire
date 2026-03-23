122afd5ff2ac89d1a35edeec63a5b5956303e689
kevinborn <kevin.born@pearlhealth.com>
2026-03-23 09:35:12 -0400

    fix seed data to comply with publishedAt constraints

--- apps/api/test/factories/action.factory.ts
+++ apps/api/test/factories/action.factory.ts
@@ -3,6 +3,7 @@ import { ActionType, EntityStatus } from '@prisma/client';
 import { generateRandomString } from './factory.utilities';
 
 export async function createAction(overrides: Partial<Prisma.ActionCreateInput> = {}) {
+  const status = overrides.status ?? EntityStatus.PUBLISHED;
   return jestPrisma.client.action.create({
     data: {
       slug: `action-${generateRandomString()}`,
@@ -10,8 +11,8 @@ export async function createAction(overrides: Partial<Prisma.ActionCreateInput> 
       summary: 'Summary',
       description: 'Description',
       actionType: ActionType.DONATE,
-      status: EntityStatus.PUBLISHED,
-      publishedAt: new Date(),
+      status,
+      publishedAt: overrides.publishedAt ?? (status === EntityStatus.PUBLISHED ? new Date() : null),
       ...overrides,
     },
   });
