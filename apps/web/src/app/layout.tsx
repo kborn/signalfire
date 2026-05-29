816e4daa7dbbc1b520f85129396b1ed8cff35d85
kevinborn <kevin.born@pearlhealth.com>
2026-05-29 14:23:52 -0400

    merriweather font

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,4 +1,4 @@
-import { Instrument_Serif, Inter } from 'next/font/google';
+import { Instrument_Serif, Merriweather } from 'next/font/google';
 import './globals.css';
 
 const instrumentSerif = Instrument_Serif({
@@ -7,15 +7,16 @@ const instrumentSerif = Instrument_Serif({
   weight: '400',
 });
 
-const inter = Inter({
+const merriweather = Merriweather({
   subsets: ['latin'],
   variable: '--font-body',
+  weight: ['300', '400', '700'],
 });
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={`${instrumentSerif.variable} ${inter.variable}`}>{children}</body>
+      <body className={`${instrumentSerif.variable} ${merriweather.variable}`}>{children}</body>
     </html>
   );
 }
