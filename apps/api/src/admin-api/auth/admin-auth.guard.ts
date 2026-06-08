c82b80b67db1c91089d3de173074825d7f46ad19
kevinborn <kevin.born@pearlhealth.com>
2026-06-08 09:22:39 -0400

    update cookie on successful request

--- apps/api/src/admin-api/auth/admin-auth.guard.ts
+++ apps/api/src/admin-api/auth/admin-auth.guard.ts
@@ -2,6 +2,8 @@ import { AdminAuthService } from './admin-auth.service';
 import { AdminAuthenticatedRequest } from './admin-auth.request';
 import { COOKIE_NAME } from '@signal-fire/api-contracts';
 import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import { AdminSession } from '@prisma/client';
+import type { Response } from 'express';
 
 @Injectable()
 export class AdminAuthGuard implements CanActivate {
@@ -9,17 +11,26 @@ export class AdminAuthGuard implements CanActivate {
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest<AdminAuthenticatedRequest>();
-    return this.validateRequest(request);
+    const session = await this.validateRequest(request);
+    const response = context.switchToHttp().getResponse<Response>();
+    response.cookie(COOKIE_NAME, session.sessionToken, {
+      httpOnly: true,
+      // TODO: make dynamic based on environment
+      secure: false,
+      sameSite: 'lax',
+      path: '/',
+      expires: session.expiresAt,
+    });
+    return true;
   }
 
-  async validateRequest(req: AdminAuthenticatedRequest): Promise<boolean> {
+  async validateRequest(req: AdminAuthenticatedRequest): Promise<AdminSession> {
     const sessionToken: unknown = req.cookies[COOKIE_NAME];
     if (typeof sessionToken !== 'string' || sessionToken.length === 0) {
       throw new UnauthorizedException('Authentication required');
     }
     const user = await this.adminAuthService.isAuthorized(sessionToken);
     req.adminUser = user;
-    await this.adminAuthService.reAuthorize(sessionToken);
-    return true;
+    return await this.adminAuthService.reAuthorize(sessionToken);
   }
 }
